513ad231c7042770f5aaa7ab9327192bbd24778a
thomas-gleizes <thomasgleizes34@gmail.com>
2021-10-01 15:06:01 +0200

    fix tailwind

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import "./assets/styles/global.css";
+import "tailwindcss/tailwind.css";
 
 ReactDOM.render(
   <BrowserRouter>
