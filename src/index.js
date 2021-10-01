e32594a2b6aad27434bcc20af223938d5a4f89a9
thomas <thomasgleizes34@gmail.com>
2021-10-01 18:21:57 +0200

    remove tailwindcss

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import "./assets/styles/global.css";
-import "tailwindcss/tailwind.css";
 
 ReactDOM.render(
   <BrowserRouter>
